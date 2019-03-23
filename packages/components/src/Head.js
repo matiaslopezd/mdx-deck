db09fd0abde2827bd5f6079cf5b4029540215035
Brent Jackson <jxnblk@gmail.com>
2019-03-23 15:55:22 -0400

    Use hooks for Head component

--- packages/components/src/Head.js
+++ packages/components/src/Head.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { createPortal } from 'react-dom'
 
 export const HeadContext = React.createContext({
@@ -16,53 +16,28 @@ export const HeadProvider = ({ tags = [], children }) => {
   return <HeadContext.Provider value={context}>{children}</HeadContext.Provider>
 }
 
-export class Head extends React.Component {
-  state = {
-    didMount: false,
-  }
-  rehydrate = () => {
-    const children = React.Children.toArray(this.props.children)
+export const Head = props => {
+  const didMount = typeof document !== 'undefined'
+  const context = useContext(HeadContext)
+  const children = React.Children.toArray(props.children).map(child =>
+    React.cloneElement(child, {
+      'data-head': true,
+    })
+  )
+
+  useEffect(() => {
     const nodes = [...document.head.querySelectorAll('[data-head]')]
     nodes.forEach(node => {
       node.remove()
     })
-    children.forEach(child => {
-      if (child.type === 'title') {
-        const title = document.head.querySelector('title')
-        if (title) title.remove()
-      }
-      if (child.type === 'meta') {
-        const { name } = child.props
-        let meta
-        if (name) meta = document.head.querySelector(`meta[name="${name}"]`)
-        if (meta) meta.remove()
-      }
-    })
-    this.setState({ didMount: true })
+  }, [children])
+
+  if (!didMount) {
+    context.push(children)
+    return false
   }
 
-  componentDidMount() {
-    this.rehydrate()
-  }
-
-  render() {
-    const children = React.Children.toArray(this.props.children).map(child =>
-      React.cloneElement(child, {
-        'data-head': true,
-      })
-    )
-    if (!this.state.didMount) {
-      return (
-        <HeadContext.Consumer
-          children={({ push }) => {
-            push(children)
-            return false
-          }}
-        />
-      )
-    }
-    return createPortal(children, document.head)
-  }
+  return createPortal(props.children, document.head)
 }
 
 export default Head
